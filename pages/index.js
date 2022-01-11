7475c11aaf22c4b182955e86ecf41806306dbc92
Dennis Acosta <email@dennisacosta.me>
2022-01-11 16:36:10 -0500

    Complete prelim desktop version

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Hero from '../components/Hero';
 import Information from '../components/Information';
 import Schedule from '../components/Schedule';
+import About from '../components/About';
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
             <Hero />
             <Information />
             <Schedule />
+            <About />
         </>
     );
 }
