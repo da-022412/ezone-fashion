f5659ef268fa4c4a296a61fc04c5c10ac96088ec
Dennis Acosta <email@dennisacosta.me>
2022-01-23 16:59:22 -0500

    Add info content

--- components/Information.js
+++ components/Information.js
@@ -10,32 +10,33 @@ const Information = () => {
             <div className={`${infoStyles['info-container']}`}>
                 <div className={`${infoStyles['title-container']}`}>
                     <h2 className={`${infoStyles['heading-2']}`}>
-                        Lorem ipsum
-                        <br />
-                        Lorem ipsum
+                        Why Attend?
                     </h2>
                     <p className={`${infoStyles['body-text']}`}>
-                        We put our attendees at the center of everything we do.
-                        We measure our success by their results. By doing this,
-                        we have successfully developed deep partnerships with
-                        manufacturers and buyers who share our values. Ezone
-                        Exhibition is inviting 500 best manufacturers throughout
-                        Asia under our corporate umbrella, ready to serve your
-                        every need. And all these factories have been verified
-                        to have good reputation, premium quality, best price,
-                        prompt delivery time, and it’s always easy to ask for
-                        samples from our 11 warehouses located throughout the
-                        USA.
+                        For this, our first Virtual Textiles Exhibbition of the
+                        year, Ezone Exhibition is inviting 500 of the best
+                        manufacturers throughout Asia to meet virtually with
+                        buyers under our corporate umbrella, and, together, we
+                        are ready to serve your every need.
                     </p>
                     <p className={`${infoStyles['body-text']}`}>
-                        These factories’ manufacturing capabilities are endless:
-                        Fabrics, apparels, curtains, carpets, bedding, cloth,
-                        hotel suppliers, shoes, hats, bags, socks, towels etc).
-                        If given the opportunity, we can meet 100% of what your
-                        needs.
+                        Ezone has already verified that each and of these
+                        factories have good reputations, premium quality, the
+                        best prices, and prompt, secure delivery. And as an
+                        added bonus, Ezone can provide samples from its eleven
+                        domestic warehouses here in the US.
                     </p>
                 </div>
                 <Icons />
+                <div className={`${infoStyles['text-container']}`}>
+                    <p className={`${infoStyles['body-text']}`}>
+                        Within the Ezone network of manufacturers, capabilities
+                        are endless: fabrics, apparel, curtatins, carpets,
+                        bedding, towels, hotel/hospitality supplies, shoes, hats
+                        bags, socks and more. Given the opportunity, we can meet
+                        100% of your textile and apparel needs.
+                    </p>
+                </div>
                 <div className={`${infoStyles['btn-container']}`}>
                     <div className={`${infoStyles['secondary-btn']}`}>
                         <Link href='#'>
