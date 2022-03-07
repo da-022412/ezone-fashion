b5bbd8edaee89c19cef7f9c195570a36e4284384
Dennis Acosta <email@dennisacosta.me>
2022-03-07 14:01:36 -0500

    Clear home

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,3 @@
-import Hero from '../components/Home/Hero';
-import Countdown from '../components/Home/Countdown';
-import Event from '../components/Home/Event';
-import Information from '../components/Home/Information';
-import Schedule from '../components/Home/Schedule';
-import Testimonials from '../components/Home/Testimonials';
-import About from '../components/Home/About';
-import Register from '../components/Home/Register';
-
 export default function Home() {
-    return (
-        <>
-            <style global jsx>{`
-                body,
-                html {
-                    margin: 0;
-                    max-width: 100vw;
-                    min-height: 100vh;
-                    overflow-x: hidden;
-                    padding: 0;
-                    scroll-behavior: smooth;
-                }
-            `}</style>
-            <Hero />
-            <Countdown />
-            <Event />
-            <Information />
-            <Schedule />
-            <Testimonials />
-            <About />
-            <Register />
-        </>
-    );
+    return <></>;
 }
