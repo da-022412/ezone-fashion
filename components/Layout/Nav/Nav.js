2db34a445a46204ddd8621a06791d5b5bd1dd6a3
Dennis Acosta <email@dennisacosta.me>
2022-03-07 13:41:42 -0500

    Add header

---
+++ components/Layout/Nav/Nav.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+import navStyles from './Nav.module.scss';
+
+const Nav = ({ links }) => {
+    return (
+        <nav className={navStyles.nav}>
+            {links.map((x) => (
+                <div className={`${navStyles['link-container']}`} key={x.id}>
+                    <Link href={x.slug} key={x.id}>
+                        <a className={`${navStyles['link-text']}`}>{x.title}</a>
+                    </Link>
+                </div>
+            ))}
+        </nav>
+    );
+};
+
+export default Nav;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,54 @@
+import Head from 'next/head';
+
+import Layout from '../components/Layout';
+
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link
+                    rel='apple-touch-icon'
+                    sizes='180x180'
+                    href='/favicon/apple-touch-icon.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='32x32'
+                    href='/favicon/favicon-32x32.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='16x16'
+                    href='/favicon/favicon-16x16.png'
+                />
+                <link rel='manifest' href='/site.webmanifest' />
+                <link
+                    rel='mask-icon'
+                    href='/favicon/safari-pinned-tab.svg'
+                    color='#5bbad5'
+                />
+                <meta name='msapplication-TileColor' content='#da532c' />
+                <meta name='theme-color' content='#ffffff' />
+                <script
+                    async
+                    src='https://www.googletagmanager.com/gtag/js?id=G-NQV8RL04SK'
+                ></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-NQV8RL04SK');`,
+                    }}
+                ></script>
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    );
 }
 
 export default MyApp;
