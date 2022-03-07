0b47a836e820f7ad81914a37b288de9d88bac2d2
Dennis Acosta <email@dennisacosta.me>
2022-03-07 13:44:38 -0500

    Switch tabs from hooks to react-tabs

--- components/Home/Schedule/MobileTimes.js
+++ components/Home/Schedule/MobileTimes.js
@@ -1,82 +1,76 @@
-import React, { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+import 'react-tabs/style/react-tabs.css';
 
 import timesStyles from './Schedule.module.scss';
 
 const MobileTimes = ({ eastern, china }) => {
-    const [isActive, setIsActive] = useState(false);
-
     return (
-        <>
-            <div className={`${timesStyles['time-tabs']}`}>
-                <div
-                    className={
-                        isActive
-                            ? `${timesStyles['time-tab']}`
-                            : `${timesStyles['time-tab-inactive']}`
+        <Tabs>
+            <TabList className={`${timesStyles['time-tabs']}`}>
+                <style global jsx>{`
+                    .react-tabs__tab--selected {
+                        background-color: #000;
+                        border: 1px solid #000;
+                        border-radius: 40px;
+                        color: #fff;
+                        font-family: 'Montserrat', sans-serif;
+                        padding: 1rem;
                     }
-                    onClick={() => setIsActive(!isActive)}
-                >
+                `}</style>
+                <Tab className={`${timesStyles['time-tab']}`}>
                     Eastern Standard Time
-                </div>
-                <div
-                    className={
-                        isActive
-                            ? `${timesStyles['time-tab']}`
-                            : `${timesStyles['time-tab-inactive']}`
-                    }
-                    onClick={() => setIsActive(!isActive)}
-                >
+                </Tab>
+                <Tab className={`${timesStyles['time-tab']}`}>
                     China Standard Time
-                </div>
-            </div>
-            {isActive ? (
-                <div className={timesStyles.time}>
-                    <div className={`${timesStyles['time-container']}`}>
-                        <div className={`${timesStyles['title-container']}`}>
-                            <h3 className={`${timesStyles['heading-3']}`}>
-                                Eastern Standard Time
-                            </h3>
-                        </div>
-                        {eastern.map((x) => (
-                            <div
-                                className={`${timesStyles['time-list']}`}
-                                key={x.id}
-                            >
-                                <span className={`${timesStyles['body-text']}`}>
-                                    {x.date}
-                                </span>
-                                <span className={`${timesStyles['body-text']}`}>
-                                    {x.content}
-                                </span>
-                            </div>
-                        ))}
+                </Tab>
+            </TabList>
+
+            <TabPanel className={timesStyles.time}>
+                <div className={`${timesStyles['time-container']}`}>
+                    <div className={`${timesStyles['title-container']}`}>
+                        <h3 className={`${timesStyles['heading-3']}`}>
+                            Eastern Standard Time
+                        </h3>
                     </div>
-                </div>
-            ) : (
-                <div className={timesStyles.time}>
-                    <div className={`${timesStyles['time-container']}`}>
-                        <div className={`${timesStyles['title-container']}`}>
-                            <h3 className={`${timesStyles['heading-3']}`}>
-                                China Standard Time
-                            </h3>
+                    {eastern.map((x) => (
+                        <div
+                            className={`${timesStyles['time-list']}`}
+                            key={x.id}
+                        >
+                            <span className={`${timesStyles['body-text']}`}>
+                                {x.date}
+                            </span>
+                            <span className={`${timesStyles['body-text']}`}>
+                                {x.content}
+                            </span>
                         </div>
-                        {china.map((x) => (
-                            <div
-                                className={`${timesStyles['time-list']}`}
-                                key={x.id}
-                            >
-                                <span className={`${timesStyles['body-text']}`}>
-                                    {x.date}
-                                </span>
-                                <span className={`${timesStyles['body-text']}`}>
-                                    {x.content}
-                                </span>
-                            </div>
-                        ))}
-                    </div>
+                    ))}
                 </div>
-            )}
-        </>
+            </TabPanel>
+            <TabPanel className={timesStyles.time}>
+                <div className={`${timesStyles['time-container']}`}>
+                    <div className={`${timesStyles['title-container']}`}>
+                        <h3 className={`${timesStyles['heading-3']}`}>
+                            China Standard Time
+                        </h3>
+                    </div>
+                    {china.map((x) => (
+                        <div
+                            className={`${timesStyles['time-list']}`}
+                            key={x.id}
+                        >
+                            <span className={`${timesStyles['body-text']}`}>
+                                {x.date}
+                            </span>
+                            <span className={`${timesStyles['body-text']}`}>
+                                {x.content}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            </TabPanel>
+        </Tabs>
     );
 };
 
