d8cf2c10a732946e48ac7447d16ba30430f24c11
Dennis Acosta <email@dennisacosta.me>
2022-03-11 11:53:33 -0500

    Dynamic content for event

--- components/Textile/Event/Event.js
+++ components/Textile/Event/Event.js
@@ -3,21 +3,16 @@ import eventStyles from './Event.module.scss';
 import Heading from '../../Heading';
 import BodyText from '../../BodyText';
 
-const CONTENT = {
-    title: 'March 22-24, 2022',
-    copy: 'Doing business with Asia&apos;s best textile and apparel manufacturers has never been easier! Now, you can meet hundreds of the best of the best, from the comfort and safety of your office, at Ezone&apos;s first Virtual Textiles and Apparel Exhibition of 2022.',
-};
-
-const Event = () => {
+const Event = ({ content }) => {
     return (
         <section className={`${eventStyles['event-section']}`}>
             <section
                 className={`${eventStyles['event-container']} ${eventStyles['text-center']}`}
             >
                 <Heading level='2' style='heading-2'>
-                    {CONTENT.title}
+                    {content.title}
                 </Heading>
-                <BodyText>{CONTENT.copy}</BodyText>
+                <BodyText>{content.copy}</BodyText>
                 <video
                     autoPlay
                     muted
