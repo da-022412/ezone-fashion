4331cacaa9206546c0dd497403de4a464af66554
Dennis Acosta <email@dennisacosta.me>
2022-03-11 11:56:47 -0500

    Make info dynamic

--- components/Textile/Information/Information.js
+++ components/Textile/Information/Information.js
@@ -5,53 +5,7 @@ import BodyText from '../../BodyText';
 
 import infoStyles from './Info.module.scss';
 
-const CONTENT = {
-    icons: [
-        {
-            img: '/images/colored-thread-bulk.jpg',
-            width: 160,
-            height: 160,
-            title: 'Textile',
-        },
-        {
-            img: '/images/young-man-rain-coat.jpg',
-            width: 160,
-            height: 160,
-            title: 'Mens',
-        },
-        {
-            img: '/images/shades-sundress.jpg',
-            width: 160,
-            height: 160,
-            title: 'Womens',
-        },
-        {
-            img: '/images/upside-down-in-bathtub.jpg',
-            width: 160,
-            height: 160,
-            title: 'Shoes',
-        },
-        {
-            img: '/images/croc-skin-purse.jpg',
-            width: 160,
-            height: 160,
-            title: 'Accessories',
-        },
-    ],
-    title: 'Access the EZone Network',
-    topCopy: [
-        {
-            copy: 'For this, our first Virtual Textiles Exhibition of the year, Ezone Exhibition is inviting 500 of the best manufacturers throughout Asia to meet virtually with buyers under our corporate umbrella, and, together, we are ready to serve your every need.',
-        },
-        {
-            copy: 'Ezone has already verified that each of these factories have good reputations, premium quality, the best prices, and prompt, secure delivery. And as an added bonus, Ezone can provide samples from its eleven domestic warehouses here in the US.',
-        },
-    ],
-    btmCopy:
-        'Within the Ezone network of manufacturers, capabilities are endless: fabrics, apparel, curtains, carpets, bedding, towels, hotel/hospitality supplies, shoes, hats bags, socks and more. Given the opportunity, we can meet 100% of your textile and apparel needs.',
-};
-
-const Information = () => {
+const Information = ({ content }) => {
     return (
         <main className={`${infoStyles['info-section']}`}>
             <div
@@ -59,15 +13,15 @@ const Information = () => {
             >
                 <div className={`${infoStyles['title-container']}`}>
                     <Heading style='heading-2' level='2'>
-                        {CONTENT.title}
+                        {content.title}
                     </Heading>
-                    {CONTENT.topCopy.map(({ copy }, index) => (
+                    {content.topCopy.map(({ copy }, index) => (
                         <BodyText key={index}>{copy}</BodyText>
                     ))}
                 </div>
-                <Icons items={CONTENT.icons} />
+                <Icons items={content.icons} />
                 <div className={`${infoStyles['text-container']}`}>
-                    <BodyText>{CONTENT.btmCopy}</BodyText>
+                    <BodyText>{content.btmCopy}</BodyText>
                 </div>
             </div>
         </main>
