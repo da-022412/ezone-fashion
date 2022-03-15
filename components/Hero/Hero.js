f159b726236ee1d87ea26fa9d34d9273283b4a2c
Dennis Acosta <email@dennisacosta.me>
2022-03-15 16:19:14 -0400

    Add video overlay

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -5,39 +5,42 @@ import Button from '../Button';
 
 const Hero = ({ content }) => {
     return (
-        <section className={heroStyles.hero}>
-            <div
-                className={`${heroStyles['hero-container']} ${heroStyles['text-center']}`}
-            >
-                <div className={`${heroStyles['title-container']}`}>
-                    <Heading level='1' style='heading-1'>
-                        {content.title}
-                    </Heading>
-                    {content.subtitle ? (
-                        <Heading level='3' style='heading-3'>
-                            {content.subtitle}
+        <>
+            <section className={heroStyles.hero}>
+                <div
+                    className={`${heroStyles['hero-container']} ${heroStyles['text-center']}`}
+                >
+                    <div className={`${heroStyles['title-container']}`}>
+                        <Heading level='1' style='heading-1'>
+                            {content.title}
                         </Heading>
-                    ) : null}
-                    {content.copy ? (
-                        <Heading level='4' style='heading-4'>
-                            {content.copy}
-                        </Heading>
-                    ) : null}
+                        {content.subtitle ? (
+                            <Heading level='3' style='heading-3'>
+                                {content.subtitle}
+                            </Heading>
+                        ) : null}
+                        {content.copy ? (
+                            <Heading level='4' style='heading-4'>
+                                {content.copy}
+                            </Heading>
+                        ) : null}
+                    </div>
+                    <Button style='primary-btn' link='#register'>
+                        Register
+                    </Button>
                 </div>
-                <Button style='primary-btn' link='#register'>
-                    Register
-                </Button>
-            </div>
-            <video
-                autoPlay
-                muted
-                loop
-                className={`${heroStyles['hero-video']}`}
-                id='heroVideo'
-            >
-                <source src={content.video} type='video/mp4' />
-            </video>
-        </section>
+                <video
+                    autoPlay
+                    muted
+                    loop
+                    className={`${heroStyles['hero-video']}`}
+                    id='heroVideo'
+                >
+                    <source src={content.video} type='video/mp4' />
+                </video>
+                <div className={`${heroStyles['hero-overlay']}`}></div>
+            </section>
+        </>
     );
 };
 
